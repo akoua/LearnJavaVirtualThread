ed9a177506f1d071e1fae18f4ac1ed183632f1af
akoua <akouayao225@gmail.com>
2024-05-13 21:35:25 +0200

    daemon thread

--- src/main/java/org/example/sec01/InBoundOutBoundTask.java
+++ src/main/java/org/example/sec01/InBoundOutBoundTask.java
@@ -1,18 +1,33 @@
 package org.example.sec01;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.stream.IntStream;
 
 public class InBoundOutBoundTask {
-    public static void main(String[] args) {
-        platformThreadDemo();
+
+    final static int NB_TASK = 10;
+
+    public static void main(String[] args) throws InterruptedException {
+        platformThreadDemo2();
     }
 
     public static void platformThreadDemo() {
 
-        IntStream.range(1, 50_001)
+        IntStream.range(1, NB_TASK)
                 .forEach(value -> new Thread(() -> Task.readFile(value))
                         .start());
     }
 
+    public static void platformThreadDemo2() throws InterruptedException {
+        var latch = new CountDownLatch(NB_TASK);
+        IntStream.range(0, NB_TASK)
+                .forEach(value -> Thread.ofPlatform().daemon()
+                        .name("daemon")
+                        .start(() -> {
+                            Task.readFile(value);
+                            latch.countDown();
+                        }));
+        latch.await();
+    }
 
 }
