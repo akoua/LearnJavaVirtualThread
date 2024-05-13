a0b4680a4bbf5af00c06531ddfeffd0d7ebbe887
akoua <akouayao225@gmail.com>
2024-05-13 21:23:49 +0200

    first commit

--- src/main/java/org/example/sec01/InBoundOutBoundTask.java
+++ src/main/java/org/example/sec01/InBoundOutBoundTask.java
@@ -1,2 +1,18 @@
-package org.example.sec01;public class InBoundOutBound {
+package org.example.sec01;
+
+import java.util.stream.IntStream;
+
+public class InBoundOutBoundTask {
+    public static void main(String[] args) {
+        platformThreadDemo();
+    }
+
+    public static void platformThreadDemo() {
+
+        IntStream.range(1, 50_001)
+                .forEach(value -> new Thread(() -> Task.readFile(value))
+                        .start());
+    }
+
+
 }
--- src/main/java/org/example/sec01/Task.java
+++ src/main/java/org/example/sec01/Task.java
@@ -1,2 +1,21 @@
-package org.example.sec01;public class Task {
+package org.example.sec01;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+
+public class Task {
+
+    final static Logger log = LoggerFactory.getLogger(Task.class);
+
+    public static void readFile(int value){
+        try {
+            log.info("Beginning task {}",value);
+            Thread.sleep(Duration.ofSeconds(1));
+            log.info("Ending task {}",value);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
