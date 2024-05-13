a9ddc99ef8d140cdecbf5e827140a1fb8a26f61f
akoua <akouayao225@gmail.com>
2024-05-13 22:10:00 +0200

    virtual thread

--- src/main/java/org/example/sec01/InBoundOutBoundTask.java
+++ src/main/java/org/example/sec01/InBoundOutBoundTask.java
@@ -5,24 +5,48 @@ import java.util.stream.IntStream;
 
 public class InBoundOutBoundTask {
 
-    final static int NB_TASK = 10;
+    final static int NB_PLATFORM_THREAD = 10_000;
+    final static int NB_VIRTAUL_THREAD = 50_000;
 
     public static void main(String[] args) throws InterruptedException {
-        platformThreadDemo2();
+        virtualThreadDemo();
     }
 
+    /**
+     * Create a simple thread with Thread Builder*
+     */
     public static void platformThreadDemo() {
 
-        IntStream.range(1, NB_TASK)
+        IntStream.range(1, NB_PLATFORM_THREAD)
                 .forEach(value -> new Thread(() -> Task.readFile(value))
                         .start());
     }
 
+    /**
+     * Create a daemon thread with Thread Builder*
+     */
     public static void platformThreadDemo2() throws InterruptedException {
-        var latch = new CountDownLatch(NB_TASK);
-        IntStream.range(0, NB_TASK)
+        var latch = new CountDownLatch(NB_PLATFORM_THREAD);
+        IntStream.range(0, NB_PLATFORM_THREAD)
                 .forEach(value -> Thread.ofPlatform().daemon()
                         .name("daemon")
+                        .start(() -> {
+                            Task.readFile(value);
+                            latch.countDown();
+                        }));
+        latch.await();
+    }
+
+    /**
+     * Create a virtual thread with Thread Builder
+     * - virtual thread is a daemon thread by default
+     */
+    public static void virtualThreadDemo() throws InterruptedException {
+
+        var latch = new CountDownLatch(NB_VIRTAUL_THREAD);
+        IntStream.range(0, NB_VIRTAUL_THREAD)
+                .forEach(value -> Thread.ofVirtual()
+                        .name("virtual-")
                         .start(() -> {
                             Task.readFile(value);
                             latch.countDown();
